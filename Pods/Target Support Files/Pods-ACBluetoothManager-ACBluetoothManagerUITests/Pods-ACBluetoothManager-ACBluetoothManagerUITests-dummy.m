15fede7710044767f4c544b80b72083c8df18641
AlexCorleone <1034131730@qq.com>
2019-11-12 15:22:08 +0800

    init bluetooth

---
+++ Pods/Target Support Files/Pods-ACBluetoothManager-ACBluetoothManagerUITests/Pods-ACBluetoothManager-ACBluetoothManagerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ACBluetoothManager_ACBluetoothManagerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ACBluetoothManager_ACBluetoothManagerUITests
+@end
---
+++ Pods/Target Support Files/Pods-ACBluetoothManager/Pods-ACBluetoothManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ACBluetoothManager : NSObject
+@end
+@implementation PodsDummy_Pods_ACBluetoothManager
+@end
